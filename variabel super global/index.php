1d88562c81dc80b23e3563ac41bdc72151f3d9a6
unknown <kfathir18@gmail.com>
2022-11-16 10:38:54 +0700

    super global

---
+++ variabel super global/index.php
@@ -0,0 +1,28 @@
+<?php
+//variabel $_server
+// berguna untuk menampilkan informasi tentang server yang digunakan
+
+// echo json_encode($_SERVER);
+
+//variabel post
+//pasing data dari body request
+
+//variable session
+//varabel yang menyimpan data pemgguna, jadi jika pengguna mengunjungi situs tersebut dengan waktu tertentu, data akan kembali tersaji tanpa harus di isi kembali
+//session bisa di atur waktunya, secara defaultnya 24 menit
+
+//variabel cookie
+//durasinya lebih panjang dari session
+
+//variabel request
+//menyimpan gabungan nilai dari $_get,post,cookie, yang di kirim bersamaan dengan request user
+
+//variabel files
+//biasanya digunakan untuk fitur upload file
+
+//variabel env
+//digunakan dalam framework yang berisi database env
+
+//variabel global
+// boar bisa akses ke semua file
+?>
